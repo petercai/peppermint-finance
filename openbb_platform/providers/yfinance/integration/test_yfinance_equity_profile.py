90ddb48f6aa536bf8ca37da819517e4f4afeec77
Peter Cai <petercaica@hotmail.com>
2025-03-15 01:19:39 -0400

    test_yfinance_equity_profile_fetcher

--- openbb_platform/providers/yfinance/integration/test_yfinance_equity_profile.py
+++ openbb_platform/providers/yfinance/integration/test_yfinance_equity_profile.py
@@ -2,8 +2,12 @@
 
 from datetime import date, timedelta
 
+import pytest
+
 from openbb_core.app.service.user_service import UserService
+from openbb_yfinance import YFinanceEquityProfileFetcher
 from openbb_yfinance.models.equity_historical import YFinanceEquityHistoricalFetcher, YFinanceEquityHistoricalData
+from openbb_yfinance.models.equity_profile import YFinanceEquityProfileData
 from openbb_yfinance.utils.helpers import yf_download
 
 test_credentials = UserService().default_user_settings.credentials.model_dump(
@@ -18,7 +22,7 @@ log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 
 # Configure logging with RotatingFileHandler
-log_file = os.path.join(log_dir, "price.log")
+log_file = os.path.join(log_dir, "profile.log")
 handler = RotatingFileHandler(
     log_file,
     maxBytes=1_000_000,  # 1MB
@@ -34,7 +38,28 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
+@pytest.mark.asyncio
+async def test_yfinance_equity_profile_fetcher():
+    """Test the yfinance Equity Profile fetcher."""
+    params = {
+        "symbol": "BNS.TO",
+    }
 
+    fetcher = YFinanceEquityProfileFetcher()
+    result = await fetcher.fetch_data(params, credentials=test_credentials)
+
+    assert result
+    # assert isinstance(result, YFinanceEquityProfileData)
+
+    # Validate some expected fields
+    # assert isinstance(result.symbol, str)
+    # assert isinstance(result.longName, str)
+    # assert isinstance(result.sector, str)
+    # assert isinstance(result.industry, str)
+    # assert isinstance(result.country, str)
+    # assert isinstance(result.marketCap, int)
+
+    logger.info(result)
 
 
 
